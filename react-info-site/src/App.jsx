cc70317b88e93942cde393c3450ee9dd13251dba
Brennen Bogue <brennen.bogueca2023@gmail.com>
2024-02-20 15:33:38 -0500

    react info bvt assignment

---
+++ react-info-site/src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react"
+import Navbar from "./components/Navbar"
+import Main from "./components/main"
+import './App.css'
+export default function App() {
+    return (
+        <div className="container">
+            <Navbar />
+            <Main />
+        </div>
+    )
+}
